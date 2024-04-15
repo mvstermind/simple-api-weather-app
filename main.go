c427ab8873811737966f4d7e2615996100af7110
mvstermind <zablockidawid1@gmail.com>
2024-04-15 20:16:59 +0200

    sends request and prints out unformatted json file

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"weather-app/api"
+)
+
+var url string = "https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&appid=%s"
+
 func main() {
 
+	fmt.Printf("Type a city to get info about weather: ")
+	b := bufio.NewReader(os.Stdin)
+
+	cityName, err := b.ReadString('\n')
+	cityName = strings.TrimSpace(cityName)
+	frmtdUrl := fmt.Sprintf(url, cityName, api.ApiKey)
+
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return
+	}
+	res, err := http.Get(frmtdUrl)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return
+	}
+	fmt.Print(string(body))
 }
